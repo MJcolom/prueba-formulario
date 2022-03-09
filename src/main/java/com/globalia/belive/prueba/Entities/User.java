11d05f75771d3d3420baf81c2a2464a73e18f882
Miguel Jesús Colom Palacios <mjcolom14@gmail.com>
2022-03-09 03:18:11 +0100

    push test

---
+++ src/main/java/com/globalia/belive/prueba/Entities/User.java
@@ -0,0 +1,119 @@
+package com.globalia.belive.prueba.Entities;
+
+
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class User {
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @Size(min = 3, max = 50)
+    private String lastname;
+
+    @NotBlank
+    @Email(message = "Please enter a valid e-mail address")
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 15)
+    private String password;
+
+    @NotBlank(message = "Las contraseñas no coinciden")
+    @Size(min = 8, max = 15)
+    private String confirmPassword;
+
+    @NotBlank
+    @Size(min = 9, max = 9)
+    @Pattern(regexp = "[0-9]{9}", message = "Formato incorrecto para telefono")
+    private String phoneNumber;
+
+    @NotNull(message = "Añada una fecha")
+    @Past(message = "La fecha deberá ser una fecha anterior a la actual")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        checkPassword();
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        checkPassword();
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    private void checkPassword() {
+        if(this.password == null || this.confirmPassword == null){
+            return;
+        }else if(!this.password.equals(confirmPassword)){
+            this.confirmPassword = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", birthday=" + birthDate +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/globalia/belive/prueba/controllers/HomeController.java
@@ -0,0 +1,44 @@
+package com.globalia.belive.prueba.controllers;
+
+import com.globalia.belive.prueba.Entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+	@Autowired
+	private CacheManager cacheManager;
+
+	@GetMapping (value = { "/" })
+	public ModelAndView loadFormPage(HttpServletRequest request) {
+		User user = new User();
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("html/form-page");
+		modelAndView.addObject(user);
+
+		return modelAndView;
+	}
+
+	@PostMapping("/register")
+	public String submitForm(@Valid @ModelAttribute("user") User user,
+							 BindingResult bindingResult, Model model) {
+		System.out.println(user);
+
+		if (bindingResult.hasErrors()) {
+			return "html/form-page";
+		} else {
+			return "html/succes-register";
+		}
+	}
+
+}
